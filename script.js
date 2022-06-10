272c04688f1834bd9e3627e1a6be7c9cefb59aa9
Ikem Nomso <ikemnomso@gmail.com>
2022-06-10 23:59:03 +0100

    add validation for input & formatting for output

--- script.js
+++ script.js
@@ -1,5 +1,26 @@
-const add = (a, b) => a + b;
-const divide = (a, b) => a / b;
-const subtract = (a, b) => a - b;
-const multiply = (a, b) => a * b;
+const formatter = new Intl.NumberFormat("en-GB");
 
+const validateInput = (a, b) => {
+  return isNaN(a) || isNaN(b) ? false : true;
+};
+
+const add = (a, b) => {
+  let valid = validateInput(a, b);
+  console.log(valid ? formatter.format(a + b) : "Error");
+};
+const divide = (a, b) => {
+  let valid = validateInput(a, b);
+  console.log(valid ? formatter.format(a / b) : "Error");
+};
+const subtract = (a, b) => {
+  let valid = validateInput(a, b);
+  console.log(valid ? formatter.format(a - b) : "Error");
+};
+const multiply = (a, b) => {
+  let valid = validateInput(a, b);
+  console.log(valid ? formatter.format(a * b) : "Error");
+};
+
+const operate = (op, x, y) => {
+  op(x, y);
+};
